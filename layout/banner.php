2d915effb36f61d3f110f8c379f2ed8c7857170d
Morgan Hein <code@morganhein.com>
2013-05-28 16:35:08 -0700

    Resized banner height.

--- layout/banner.php
+++ layout/banner.php
@@ -1,5 +1,5 @@
 <div class="navbar navbar-inverse navbar-fixed-top">
-  <div class="navbar-inner">
+  <div class="navbar-inner" id="navbar">
    <div class="container-fluid">
     <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
       <span class="icon-bar"></span>
