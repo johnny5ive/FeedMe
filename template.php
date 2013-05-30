5154b25446afe3187f4fee8f3dbfdbdd20a23cb3
Morgan Hein <code@morganhein.com>
2013-05-30 14:10:45 -0700

    Created the user panel on the right with styles. Basics in place.

--- template.php
+++ template.php
@@ -7,9 +7,18 @@
     <div class="container-fluid">
      
       <div class="row-fluid">
-        <div class="span12" id="content">
+        <div class="span10" id="content">
+          content
         </div>
-        <div class="span3" id="sidebar">
+        <div class="span2" id="sidebar">
+          <div id="side_panel">
+            <div class="panel" id="user_panel">
+              connected services<br>
+              profile image - name<br>
+            </div>
+            <div class="panel" id="friends_screen">Friend liked your mom.</div>
+            <div class="panel" id="user_info">This person is active!</div>
+          </div>
         </div>
       </div>
     
