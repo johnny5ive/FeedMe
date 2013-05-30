0832f499ed1dd87358f7ff5dac9ccb4f9534fc37
Morgan Hein <code@morganhein.com>
2013-05-30 16:07:07 -0700

    Finished user panel and recent activity. Added logos for services.

--- template.php
+++ template.php
@@ -7,18 +7,50 @@
     <div class="container-fluid">
      
       <div class="row-fluid">
-        <div class="span10" id="content">
+        <div class="span9" id="content">
           content
         </div>
-        <div class="span2" id="sidebar">
+        <div class="span3" id="sidebar">
           <div id="side_panel">
             <div class="panel rounded-top" id="user_panel">
-              
-              connected services<br>
-              profile image - name<br>
+              <img src="static/img/default.jpg" class="user-pic">
+              USERNAME
+              <div id="connected-services">
+                <img src="static/img/facebook.png" class="service-image" />
+                <img src="static/img/linkedin.png" class="service-image" />
+                <img src="static/img/googleplus.png" class="service-image" />
+                <div id="notification-image">122</div>
+              </div>
             </div>
-            <div class="panel" id="friends_panel">Friend liked your mom.</div>
-            <div class="panel rounded-bottom" id="user_info">This person is active!</div>
+            <div class="panel" id="friends_panel">
+              <div class="friend-activity">
+                <img src="static/img/default.jpg" class="activity-pic"><span class="activity-title">Article Name</span><br />
+                <span class="activity-user">Friend A</span> <span class="activity-description">commented on the following article!</span>
+              </div>
+            <div class="clearfloats">
+            <div class="friend-activity">
+                <img src="static/img/default.jpg" class="activity-pic"><span class="activity-title">Article Name</span><br />
+                <span class="activity-user">Friend A</span> <span class="activity-description">commented on the following article!</span>
+              </div>
+            </div>
+            <div class="clearfloats">
+            <div class="friend-activity">
+                <img src="static/img/default.jpg" class="activity-pic"><span class="activity-title">Article Name</span><br />
+                <span class="activity-user">Friend A</span> <span class="activity-description">commented on the following article!</span>
+              </div>
+            </div>
+            <div class="clearfloats">
+            <div class="friend-activity">
+                <img src="static/img/default.jpg" class="activity-pic"><span class="activity-title">Article Name</span><br />
+                <span class="activity-user">Friend A</span> <span class="activity-description">commented on the following article!</span>
+              </div>
+            </div>
+
+            </div>
+            <div class="panel rounded-bottom" id="actives_panel">
+              <span id="active-friends">Active Friends</span>
+              
+            </div>
           </div>
         </div>
       </div>
