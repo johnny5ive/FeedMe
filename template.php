ef7714855e329dc013c6f92c0153978fa304b81d
Morgan Hein <code@morganhein.com>
2013-06-02 14:09:10 -0700

    Adding controls to the right of an article.

--- template.php
+++ template.php
@@ -10,18 +10,21 @@
         <div class="span9" id="content">
           <article>
             <img src="static/img/default.jpg" class="article-image" />
+            <?php include 'layout/controls.php' ?>
             <div class="title">@Article.title</div>
             <div class="tagline">@Author.name | @Article.timestamp | @Article.articleType | @LIST(Categories.name) </div>
             <div>@Article.abstract: Synergistic visionary reinvent strategic channels visionary clicks-and-mortar back-end aggregate. Weblogs leverage embedded. </div>
           </article>
           <article>
-            <img src="static/img/default.jpg" class="article-image" />
+              <img src="static/img/default.jpg" class="article-image" />
+              <?php include 'layout/controls.php' ?>
               <div class="title">@Article.title</div>
               <div class="tagline">@Author.name | @Article.timestamp | @Article.articleType | @LIST(Categories.name)</div>
               <div>@Article.abstract: Synergistic visionary reinvent strategic channels visionary clicks-and-mortar back-end aggregate. Weblogs leverage embedded. </div>
           </article>
           <article>
-            <img src="static/img/default.jpg" class="article-image" />
+              <img src="static/img/default.jpg" class="article-image" />
+              <?php include 'layout/controls.php' ?>
               <div class="title">@Article.title</div>
               <div class="tagline">@Author.name | @Article.timestamp | @Article.articleType | @LIST(Categories.name)</div>
               <div>@Article.abstract: Synergistic visionary reinvent strategic channels visionary clicks-and-mortar back-end aggregate. Weblogs leverage embedded. </div>
