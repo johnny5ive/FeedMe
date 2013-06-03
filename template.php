0d98a99506b48a605a4fe76a13d18785759313cc
Morgan Hein <code@morganhein.com>
2013-06-03 01:33:17 -0700

    Creating templates.

--- template.php
+++ template.php
@@ -31,60 +31,7 @@
           </article>
         </div>
         <div class="span3" id="sidebar">
-          <div id="side_panel">
-            <div class="panel rounded-top" id="user_panel">
-              <img src="static/img/default.jpg" class="user-pic">
-              USERNAME
-              <div id="connected-services">
-                <img src="static/img/facebook.png" class="service-image" />
-                <img src="static/img/linkedin.png" class="service-image" />
-                <img src="static/img/googleplus.png" class="service-image" />
-                <div id="notification-image">122</div>
-              </div>
-            </div>
-            <div class="panel" id="friends_panel">
-              <div class="friend-activity">
-                <img src="static/img/default.jpg" class="activity-pic"><span class="activity-title">Article Name</span><br />
-                <span class="activity-user">Friend A</span> <span class="activity-description">commented on the following article!</span>
-              </div>
-              <div class="clearfloats">
-              <div class="friend-activity">
-                  <img src="static/img/default.jpg" class="activity-pic"><span class="activity-title">Article Name</span><br />
-                  <span class="activity-user">Friend A</span> <span class="activity-description">commented on the following article!</span>
-                </div>
-              </div>
-              <div class="clearfloats">
-              <div class="friend-activity">
-                  <img src="static/img/default.jpg" class="activity-pic"><span class="activity-title">Article Name</span><br />
-                  <span class="activity-user">Friend A</span> <span class="activity-description">commented on the following article!</span>
-                </div>
-              </div>
-              <div class="clearfloats">
-              <div class="friend-activity">
-                <img src="static/img/default.jpg" class="activity-pic"><span class="activity-title">Article Name</span><br />
-                <span class="activity-user">Friend A</span> <span class="activity-description">commented on the following article!</span>
-              </div>
-            </div>
-
-            </div>
-            <div class="panel rounded-bottom" id="actives_panel">
-              <span id="active-friends">Active Friends</span>
-              <div class="carousel-wrapper">
-                <ul id="friends-activity-carousel" class="jcarousel-skin-tango">
-                  <li><img src="static/img/default.jpg" class="carousel-img" /></li>
-                  <li><img src="static/img/default.jpg" class="carousel-img" /></li>
-                  <li><img src="static/img/default.jpg" class="carousel-img" /></li>
-                  <li><img src="static/img/default.jpg" class="carousel-img" /></li>
-                  <li><img src="static/img/feedme.png" class="carousel-img"/></li>
-                  <li><img src="static/img/default.jpg" class="carousel-img" /></li>
-                  <li><img src="static/img/default.jpg" class="carousel-img" /></li>
-                  <li><img src="static/img/default.jpg" class="carousel-img" /></li>
-                  <li><img src="static/img/default.jpg" class="carousel-img" /></li>
-                  <li><img src="static/img/feedme.png" class="carousel-img"/></li>
-                </ul>
-              </div>
-            </div>
-          </div>
+          <?php include 'layout/user_sidebar.php' ?>
         </div>
       </div>
     
