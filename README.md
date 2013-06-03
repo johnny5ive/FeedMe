14c0656d9fe9fb2a5e3f9dab24185fa956491428
Morgan Hein <code@morganhein.com>
2013-06-03 02:26:21 -0700

    Updated readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Howdy!
+This is a website designed for INFO 330 at the University of Washington.
+
+It was created by Morgan Hein, Jordan Atwood, and Kartik Rishi.
+
+A full working example can be found at: http://morganhein.com/feedme
--- layout/template_user_sidebar.php
+++ layout/template_user_sidebar.php
@@ -1,6 +1,6 @@
   <div id="side_panel">
     <div class="panel rounded-top" id="user_panel">
-      <img src="static/img/{{user.userPic}}" class="user-pic">
+      <img src="static/img/{{users.picture}}" class="user-pic">
       {%% users.name %%}
       <div id="connected-services">
         <img src="static/img/facebook.png" class="service-image" />
@@ -12,8 +12,8 @@
     <div class="panel" id="friends_panel">
       {% for activity in activities(userID) %}
       <div class="friend-activity">
-        <img src="thumbnails/users/{{activity.userPic}}" class="activity-pic"><span class="activity-title">{{activity.title}}</span><br />
-        <span class="activity-user">{{activity.user}}</span> <span class="activity-description">commented on the above article!</span>
+        <img src="thumbnails/users/{{activity.picture}}" class="activity-pic"><span class="activity-title">{{activity.title}}</span><br />
+        <span class="activity-user">{{activity.name}}</span> <span class="activity-description">commented on the above article!</span>
       </div>
       <div class="clearfloats"></div>
       {% endfor %}
@@ -23,8 +23,8 @@
       <span id="active-friends">Active Friends</span>
       <div class="carousel-wrapper">
         <ul id="friends-activity-carousel" class="jcarousel-skin-tango">
-          {% for friend in activeFriends %}
-          <li><img src="thumbnails/users/{friend.userPic}" class="carousel-img" /></li>
+          {% for friend in activities(userID) %}
+          <li><img src="thumbnails/users/{friend.picture}" class="carousel-img" /></li>
           {% endfor %}
         </ul>
       </div>
